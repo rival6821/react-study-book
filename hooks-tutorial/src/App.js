80c7c649a0ecb45485255d6fb7aa4862a0c69f3e
ilhoon <rival6821@naver.com>
2019-12-10 22:46:23 +0900

    useReducer로 input 처리

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Counter from './Counter';
+import Info from './Info';
 
 const App = () => {
   return (
     <div>
-      <Counter></Counter>
+      <Info></Info>
     </div>
   );
 };
