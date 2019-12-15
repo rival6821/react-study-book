06dea89bce794186a586add67411a0531866642a
ilhoon <rival6821@naver.com>
2019-12-15 15:48:53 +0900

    9장완료

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <CSSModule></CSSModule>
+        <StyledComponent></StyledComponent>
       </div>
     );
   }
