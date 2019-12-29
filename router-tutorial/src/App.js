20636feefd97821b739f6184bcc9b869aaace1a3
ilhoon <rival6821@naver.com>
2019-12-29 20:25:34 +0900

    라우트, 쿼리

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile';
 
 const App = () => {
   return (
@@ -13,10 +14,17 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/ilhoon">이일훈 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">홍길동 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };
