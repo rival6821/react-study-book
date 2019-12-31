0de5dc0d14050a7ec566c7abf533d641029af98c
ilhoon <rival6821@naver.com>
2019-12-31 21:28:09 +0900

    라우터 내부 라우터

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 const App = () => {
   return (
@@ -15,16 +15,13 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/ilhoon">이일훈 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">홍길동 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 };
